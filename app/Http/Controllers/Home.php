52863ca9d0d5e45eb029c91f99c44d02d5c740bf
syamsulapp <samsul.marif100@gmail.com>
2021-07-31 11:44:17 +0800

    template untuk frontend, dan bikin logic untuk menu front endnya

---
+++ app/Http/Controllers/Home.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Http\Controllers\Controller;
+
+class Home extends Controller
+{
+    public function index()
+    {
+        return view('halo selamat datang di Gravitasi apps 2021');
+    }
+
+    public function home()
+    {
+    }
+
+    public function about()
+    {
+    }
+
+    public function kontak()
+    {
+    }
+
+    public function login()
+    {
+    }
+
+    public function register()
+    {
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use SebastianBergmann\CodeCoverage\CrapIndex;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', [Home::class, 'index']);
